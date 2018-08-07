c65ddc67d0c059406ec613c1464b63fb1e6f1bbb
ljjde1981 <ljjde1981@gmail.com>
2018-08-07 19:20:02 +0900

    no message

--- src/main/java/com/luo/OkServlet.java
+++ src/main/java/com/luo/OkServlet.java
@@ -1,4 +1,18 @@
 package com.luo;
 
-public class OkServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="HelloServlet2", urlPatterns={"/hello2"})
+public class OkServlet  extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.getWriter().print("Hello!");
+    }
 }
